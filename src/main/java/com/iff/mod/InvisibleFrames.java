04dfae9ba3cdfd00ce40b319b8042ec621f30d63
acaeaeeda <147932550+acaeaeeda@users.noreply.github.com>
2024-05-04 17:26:39 +0800

    1.18.2

---
+++ src/main/java/com/iff/mod/InvisibleFrames.java
@@ -0,0 +1,40 @@
+package com.iff.mod;
+
+
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+
+import org.slf4j.Logger;
+import com.mojang.logging.LogUtils;
+
+import net.minecraftforge.event.entity.player.PlayerInteractEvent.EntityInteract;
+
+
+
+
+@Mod(InvisibleFrames.MODID)
+public class InvisibleFrames {
+    public static final String MODID = "iff";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE,modid = MODID)
+    public static class FrameInteract
+    {
+        @SubscribeEvent
+        public static void onFrameInteract(EntityInteract event){
+            if (event == null || event.getTarget() == null){return;}
+            if (event.getTarget().getType().toString().equalsIgnoreCase("entity.minecraft.item_frame") || event.getTarget().getType().toString().equalsIgnoreCase("entity.minecraft.glow_item_frame"))
+            {
+                    if (event.getEntity().isShiftKeyDown()){
+                        event.getTarget().setInvisible(false);
+                        event.setCanceled(true);
+
+                    }else{
+                        event.getTarget().setInvisible(true);
+                }
+            }
+        }
+    }
+}
